c615357aa8d1c2cb53e6fc16a34432a78def2bab
Volodymyr Bashev <vladimirbashev@gmail.com>
2025-04-30 12:09:07 +0200

    user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,25 +6,10 @@ export class UserController extends BaseController {
     constructor(logger: LoggerService) {
         super(logger);
         this.bindRoutes([
-            {
-                path: '/login',
-                func: this.login,
-                method: 'post',
-                method2: 'post'
-            },
-            {
-                path: '/register',
-                func: this.login,
-                method: 'post',
-                method2: 'post'
-            },
-            {
-                path: '',
-                func: this.root,
-                method: 'get',
-                method2: 'get'
-            }
-            ])
+            { path: '/login', func: this.login, method: 'post', method2: 'post' },
+            { path: '/register', func: this.login, method: 'post', method2: 'post' },
+            { path: '', func: this.root, method: 'get', method2: 'get' }
+        ])
     }
 
     login(req: Request, res: Response, next: NextFunction) {
