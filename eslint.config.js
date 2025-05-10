7a1200df218c71969c9917df8671fc72031c8a5a
Volodymyr Bashev <vladimirbashev@gmail.com>
2025-05-10 13:40:00 +0200

    es-lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "eslint/config";
 import typescriptParser from "@typescript-eslint/parser";
 import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 
-export default defineConfig([
+export default [
+    { ignores: ['dist', 'node_modules'] },
     {
         files: ['**/*.ts', '**/*.tsx'],
         languageOptions: {
             parser: typescriptParser,
             parserOptions: {
+                ecmaVersion: 'latest',
                 project: './tsconfig.json',
                 sourceType: 'module',
             },
@@ -15,9 +16,19 @@ export default defineConfig([
         plugins: {
             '@typescript-eslint': typescriptPlugin,
         },
+        globals: {
+            NodeJS: true,
+            require: true,
+            module: true,
+            __dirname: true,
+        },
         rules: {
             semi: "error",
+            "no-var": "error",
             "prefer-const": "error",
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'off',
         },
     },
-]);
+];
